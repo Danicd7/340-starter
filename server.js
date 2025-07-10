d3ef680242f139625f8b1cd561cf0c2847f68efb
Dani <das22001@byui.edu>
2025-07-10 22:56:50 +0200

    commit

--- server.js
+++ server.js
@@ -5,15 +5,31 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
-const app = express()
+const app = express();
 const static = require("./routes/static")
+
+
+app.use(static)
+
+/* ***********************
+ *View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
+ *View Engine and Templates
  *************************/
 app.use(static)
+//Index route
+app.get("/", function(req, res){res.render("index", {title: "home"})})
+
+
 
 /* ***********************
  * Local Server Information
